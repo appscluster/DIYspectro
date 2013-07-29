70dcd96e35bfb22691a0b975cb2229617bf4fb69
Phil <philipp.g@gmx.de>
2013-07-29 16:05:57 +0300

    save spectra as png and pickle

--- take_measurement.py
+++ take_measurement.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 import pylab as pl
+import datetime
+import pickle
 
 '''
 Reads out open-source spectrometers, e.g. developed by 'http://spectralworkbench.org/'. Reads webcam stream for a given number of frames and averages over a few lines in the picture, as well as time. 
@@ -48,27 +50,32 @@ def take_measurement(cam, res_x, res_y, channels, time):
 
 ##############################################
 
-cam = 0 # 0: intern, 1: USB webcam
+cam = 1 # 0: intern, 1: USB webcam
 
-time = 100
+time = 10
 res_x = 1280
-res_y = 720
+res_y = 1024
 channels = 3 # RGB
 
 # choose which parts of the image are evaluated
-x_start = 0
-x_stop = res_x
+x_start = 10
+x_stop = 1000
 
-y_center = res_y/2
-y_range = 25
+y_center = 470
+y_range = 10
 
+wl_calib = (0.4317, 335.0751)  # conversion factor WL[nm] --> Readings
 ##############################################
 
 # trigger measurement
 res0, res1, res2 = take_measurement(cam, res_x, res_y, channels, time)
 
+# wavelength array
+wl = ((np.arange(res_x) - x_start) * wl_calib[0] + wl_calib[1])[x_start:x_stop]
+
 # figs and plots
-pl.figure(figsize=(10,4))
+pl.figure(0, figsize=(10,4))
+pl.clf()
 pl.subplot(211)
 pl.imshow(res1[y_center - y_range:y_center + y_range, x_start:x_stop, 0])
 pl.title('One Frame')
@@ -80,19 +87,26 @@ pl.title('Averaged over %i frames' % time)
 pl.tight_layout()
 pl.show()
 
-
 res1_1 = res1[y_center, x_start:x_stop, time/2]
 res2_1 = res2[y_center, x_start:x_stop]
 res2_2 = res2[y_center - y_range:y_center + y_range, x_start:x_stop].mean(axis=0)
 
-pl.figure()
-pl.plot(res1_1, label = 'One Row')
-pl.plot(res2_1, label = 'Time Average, One Row')
-pl.plot(res2_2, label = 'Time Average, %i Rows' % (y_range*2))
+timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+pickle.dump((res2, res2_2, wl, wl_calib), open('pickle/Spectrum_%s.pickle' % timestamp, 'w'))
+
+pl.figure(1)
+pl.clf()
+pl.plot(wl, res1_1, label = 'One Row')
+pl.plot(wl, res2_1, label = 'Time Average, One Row')
+pl.plot(wl, res2_2, label = 'Time Average, %i Rows' % (y_range*2))
+pl.xlabel('Wavelength, [nm]')
+pl.ylabel('Intensity, [counts]')
 pl.legend(loc=0)
 pl.grid()
 pl.title('Spectrum Enhancement. Averages over %i Frames' % time)
 pl.show()
+pl.savefig('plots/Spectrum_%s.png' % timestamp)
+
 
 '''
 pl.figure()
